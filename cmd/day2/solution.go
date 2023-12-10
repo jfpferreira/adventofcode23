1d582289138c45ce764e88b635df593c0d231f34
Joao Ferreira <jfp.ferreira96@gmail.com>
2023-12-10 17:58:51 +0000

    Solved day 2

--- cmd/day2/solution.go
+++ cmd/day2/solution.go
@@ -1,10 +1,112 @@
 package day2
 
-func solveA(input []string) string {
-	return "day 2 A"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Set map[string]int
+
+type Game struct {
+	id int
+	sets []*Set
 }
 
+func parse(input []string) []*Game {
+	games := []*Game{}
+	for _, line := range input {
+		parts := strings.Split(line, ":")
+
+		i, err := strconv.Atoi(strings.Split(parts[0], " ")[1]) 
+		if err != nil {
+			panic("The identifier was not a number");
+		}
+		game := &Game{
+			id: i,
+			sets: []*Set{},
+		}
+		
+		for _, set := range strings.Split(strings.TrimSpace(parts[1]), ";") {
+
+			currentSet := make(Set)
+			for _, cube_set := range strings.Split(strings.TrimSpace(set), ",") {
+				fetch := strings.Split(strings.TrimSpace(cube_set), " ")
+
+				number, err := strconv.Atoi(fetch[0])
+				if err != nil {
+					panic("The number was not")
+				}
+
+				currentSet[fetch[1]] = number
+			}
+			game.sets = append(game.sets, &currentSet)
+		}
+		games = append(games, game)
+	}
+
+	return games
+}
+
+var cubes = map[string]int{
+	"red": 12,
+	"green": 13,
+	"blue": 14,
+}
+
+func solveA(input []string) string {
+	games := parse(input)
+	
+	total := 0
+	for _, game := range games {
+		if isGameValid(game) {
+			total += game.id
+		}	
+	}
+
+	return fmt.Sprint(total)
+}
+
+func isGameValid(game *Game) bool {
+	for _, set := range game.sets {
+		for k, v := range *set {
+			if v > cubes[k] {
+				return false
+			}
+		}
+	}
+	return true
+}
 
 func solveB(input []string) string {
-	return "day 2 B"
+	games := parse(input)
+	total := 0
+
+	for _, game := range games {
+		power := 1
+		for _, v := range minimumCubes(game) {
+			power *= v
+		}
+
+		total += power
+	}
+
+	return fmt.Sprint(total)
+}
+
+func minimumCubes(game *Game) map[string]int {
+	minimumCubes := map[string]int{
+		"red": 0,
+		"green": 0,
+		"blue": 0,
+	}
+
+	for _, set := range game.sets {
+		for k, v := range *set {
+			if v > minimumCubes[k] {
+				minimumCubes[k] = v
+			}
+		}
+	}
+	return minimumCubes
 }
--- cmd/day2/solution_test.go
+++ cmd/day2/solution_test.go
@@ -1,55 +1,54 @@
 package day2
 
 import (
-	// "strconv"
+	"strconv"
 	"testing"
 )
 
 func TestA(t *testing.T){
-	// input := []string{
-	// 	"1abc2",
-	// 	"pqr3stu8vwx",
-	// 	"a1b2c3d4e5f",
-	// 	"treb7uchet",
-	// }
+	input := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
 
-	// expected := 142
+	expected := 8
 
-	// result := solveA(input)
-	// i, err := strconv.Atoi(result) 
-	// if err != nil {
-	// 	t.Fatal("Result was not an int");
-	// 	return
-	// }
+	result := solveA(input)
+	i, err := strconv.Atoi(result) 
+	if err != nil {
+		t.Fatal("Result was not an int");
+		return
+	}
 
-	// if i != expected{
-	// 	t.Fatalf("Expected: %d, Got: %d", expected, i)
-	// 	return
-	// }
+	if i != expected{
+		t.Fatalf("Expected: %d, Got: %d", expected, i)
+		return
+	}
 }
 
 func TestB(t *testing.T){
-	// input := []string{
-	// 	"two1nine",
-	// 	"eightwothree",
-	// 	"abcone2threexyz",
-	// 	"xtwone3four",
-	// 	"4nineeightseven2",
-	// 	"zoneight234",
-	// 	"7pqrstsixteen",
-	// }
+	input := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
 
-	// expected := 281
+	expected := 2286
 
-	// result := solveB(input)
-	// i, err := strconv.Atoi(result) 
-	// if err != nil {
-	// 	t.Fatal("Result was not an int");
-	// 	return
-	// }
+	result := solveB(input)
+	i, err := strconv.Atoi(result) 
+	if err != nil {
+		t.Fatal("Result was not an int");
+		return
+	}
 
-	// if i != expected{
-	// 	t.Fatalf("Expected: %d, Got: %d", expected, i)
-	// 	return
-	// }
+	if i != expected{
+		t.Fatalf("Expected: %d, Got: %d", expected, i)
+		return
+	}
 }
